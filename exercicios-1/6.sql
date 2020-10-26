54fb4feed2f34ca5ab713e3a8e084f6bb46fda62
GiovanniTorres0 <73203847+GiovanniTorres0@users.noreply.github.com>
2020-10-26 20:36:38 -0300

    Create 6.sql

---
+++ exercicios-1/6.sql
@@ -0,0 +1,21 @@
+create database exerc;
+
+CREATE TABLE employs (
+id int NOT NULL AUTO_INCREMENT,
+nome varchar(30) not null,
+nascimento date,
+sexo enum('M', 'F'),
+peso decimal (5,2),
+altura decimal(3,2),
+nacionalidade varchar(20) DEFAULT 'Brasil',
+primary key (id)
+)default charset = utf8;
+
+insert into employs values
+(DEFAULT, 'carlos', '1993-04-10', 'M', '70.2', '1.77', DEFAULT),
+(DEFAULT, 'bruna', '1992-01-01', 'F', '60.2', '1.50', DEFAULT),
+(DEFAULT, 'Bianca', '1990-04-17', 'F', '75.2', '2.00', DEFAULT);
+
+select DISTINCT nome from employs WHERE nome > 10001 ;
+
+
